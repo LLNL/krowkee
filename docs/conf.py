59e332b448e42559b6d700ed0e9f1076e5c7a98d
Benjamin W. Priest (they/theym) <bwpriester@gmail.com>
2023-03-29 16:44:07 -0700

    Added hooks for RTD to build docs server-side

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,32 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import subprocess
+
+
+def configureDoxyfile(input_dir, output_dir):
+    with open("Doxyfile.in", "r") as file:
+        filedata = file.read()
+
+    filedata = filedata.replace("@DOXYGEN_INPUT_DIR@", input_dir)
+    filedata = filedata.replace("@DOXYGEN_OUTPUT_DIR@", output_dir)
+
+    with open("Doxyfile", "w") as file:
+        file.write(filedata)
+
+
+# check if we are running on ReadTheDoc's servers
+read_the_docs_build = os.environ.get("REDADTHEDOCS", None) == "True"
+
+breathe_projects = {}
+if read_the_docs_build:
+    input_dir = "../input"
+    output_dir = "build"
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call("doxygen", shell=True)
+    breathe_projects["krowkee"] = output_dir + "/xml"
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
