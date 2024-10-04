50af428674a1555f5c537f2664798c59b096db0e
Min W. Priest (they/them) <bwpriester@gmail.com>
2024-10-03 17:12:29 -0700

    removed ygm as a library dependency (only used as an optional test dependency)

--- test/include/local_sketches.hpp
+++ test/include/local_sketches.hpp
@@ -8,27 +8,26 @@
 #include <krowkee/util/tests.hpp>
 
 using Dense32CountSketch =
-    krowkee::sketch::LocalCountSketch<krowkee::sketch::Dense, std::int32_t>;
+    krowkee::sketch::CountSketch<krowkee::sketch::Dense, std::int32_t>;
 
 using MapSparse32CountSketch =
-    krowkee::sketch::LocalCountSketch<krowkee::sketch::MapSparse32,
-                                      std::int32_t>;
+    krowkee::sketch::CountSketch<krowkee::sketch::MapSparse32, std::int32_t>;
 
 using MapPromotable32CountSketch =
-    krowkee::sketch::LocalCountSketch<krowkee::sketch::MapPromotable32,
-                                      std::int32_t>;
+    krowkee::sketch::CountSketch<krowkee::sketch::MapPromotable32,
+                                 std::int32_t>;
 
 #if __has_include(<boost/container/flat_map.hpp>)
 using FlatMapSparse32CountSketch =
-    krowkee::sketch::LocalCountSketch<krowkee::sketch::FlatMapSparse32,
-                                      std::int32_t>;
+    krowkee::sketch::CountSketch<krowkee::sketch::FlatMapSparse32,
+                                 std::int32_t>;
 
 using FlatMapPromotable32CountSketch =
-    krowkee::sketch::LocalCountSketch<krowkee::sketch::FlatMapPromotable32,
-                                      std::int32_t>;
+    krowkee::sketch::CountSketch<krowkee::sketch::FlatMapPromotable32,
+                                 std::int32_t>;
 #endif
 
-using Dense32FWHT = krowkee::sketch::LocalFWHT<std::int32_t>;
+using Dense32FWHT = krowkee::sketch::FWHT<std::int32_t>;
 
 template <typename T>
 using make_ptr_functor_t = make_shared_functor_t<T>;
