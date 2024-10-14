803a13c27c9443f74f649aad50ef327516978967
Min W. Priest (they/them) <bwpriester@gmail.com>
2024-10-14 13:57:48 -0700

    made internal hash variable names human readable

--- include/krowkee/hash/hash.hpp
+++ include/krowkee/hash/hash.hpp
@@ -21,29 +21,32 @@ namespace hash {
  * Hash functor base class
  *
  * Right now only supports hashing to a power of 2 range.
+ *
+ * @tparam Range The desired range for the hash function. Gets rounded up to the
+ * nearest power of 2.
  */
 struct Base {
   /**
    * @param range the desired range for the hash function. Currently we round up
-   * to the nearest power of 2 for `_m'.
+   * to the nearest power of 2 for `_range'.
    * @param seed the random seed controlling any randomness. Might be ignored.
    */
   Base(const std::uint64_t range, const std::uint64_t seed = default_seed)
-      : _m(64 - ceil_log2_64(range)), _seed(seed) {}
+      : _range(64 - ceil_log2_64(range)), _seed(seed) {}
 
   Base() {}
 
   /**
-   * Truncate a hashed 64-bit value to the desired range specified by `_m`.
+   * Truncate a hashed 64-bit value to the desired range specified by `_range`.
    *
    * @param val the hashed value to be truncated.
    */
   constexpr std::uint64_t truncate(const std::uint64_t val) const {
-    return val >> _m;
+    return val >> _range;
   }
 
-  constexpr std::uint64_t get_m() const { return _m; }
-  constexpr std::size_t   size() const { return _1u64 << (64 - _m); }
+  constexpr std::uint64_t get_range() const { return _range; }
+  constexpr std::size_t   size() const { return _1u64 << (64 - _range); }
   constexpr std::size_t   seed() const { return _seed; }
   inline std::string      state() const {
     std::stringstream ss;
@@ -52,21 +55,21 @@ struct Base {
   }
 
   void swap(Base &rhs) {
-    std::swap(_m, rhs._m);
+    std::swap(_range, rhs._range);
     std::swap(_seed, rhs._seed);
   }
 
 #if __has_include(<cereal/types/base_class.hpp>)
   template <class Archive>
   void serialize(Archive &archive) {
-    archive(_m, _seed);
+    archive(_range, _seed);
   }
 #endif
 
   friend void swap(Base &lhs, Base &rhs) { lhs.swap(rhs); }
 
  protected:
-  std::uint64_t              _m;
+  std::uint64_t              _range;
   std::uint64_t              _seed;
   const static std::uint64_t _1u64 = 1;
 };
@@ -103,7 +106,7 @@ struct WangHash : public Base {
   }
 
   friend constexpr bool operator==(const WangHash &lhs, const WangHash &rhs) {
-    return lhs._m == rhs._m && lhs._seed == rhs._seed;
+    return lhs._range == rhs._range && lhs._seed == rhs._seed;
   }
 
   friend constexpr bool operator!=(const WangHash &lhs, const WangHash &rhs) {
@@ -139,9 +142,9 @@ struct WangHash : public Base {
  */
 struct MulShift : public Base {
   /**
-   * Initialize `_a` given the specified random `seed`.
+   * Initialize `_multiplicand` given the specified random `seed`.
    *
-   * `_a` is randomly sampled from [0, 2^64 - 1].
+   * `_multiplicand` is randomly sampled from [0, 2^64 - 1].
    *
    * @tparam ARGS types of additional (ignored) hash functor arguments.
    *
@@ -155,17 +158,17 @@ struct MulShift : public Base {
     std::mt19937_64                              rnd_gen(wang64(_seed));
     std::uniform_int_distribution<std::uint64_t> udist(
         1, std::numeric_limits<std::uint64_t>::max());
-    std::uint64_t a;
+    std::uint64_t multiplicand;
     do {
-      a = udist(rnd_gen);
-    } while (is_even(a));
-    _a = a;
+      multiplicand = udist(rnd_gen);
+    } while (is_even(multiplicand));
+    _multiplicand = multiplicand;
   }
 
   MulShift() : Base() {}
 
   /**
-   * Compute `_a * x mod _m`.
+   * Compute `_multiplicand * x mod _range`.
    *
    * @tparam OBJ the object to be hashed. Presently must fit into a 64-bit
    *     register.
@@ -174,7 +177,7 @@ struct MulShift : public Base {
    */
   template <typename OBJ>
   constexpr std::uint64_t operator()(const OBJ &x) const {
-    return truncate(_a * x);
+    return truncate(_multiplicand * x);
   }
 
   /**
@@ -184,17 +187,18 @@ struct MulShift : public Base {
 
   inline std::string state() const {
     std::stringstream ss;
-    ss << Base::state() << ", multiplicand: " << _a;
+    ss << Base::state() << ", multiplicand: " << _multiplicand;
     return ss.str();
   }
 
   friend void swap(MulShift &lhs, MulShift &rhs) {
-    std::swap(lhs._a, rhs._a);
+    std::swap(lhs._multiplicand, rhs._multiplicand);
     lhs.swap(rhs);
   }
 
   friend constexpr bool operator==(const MulShift &lhs, const MulShift &rhs) {
-    return lhs._m == rhs._m && lhs._seed == rhs._seed && lhs._a == rhs._a;
+    return lhs._range == rhs._range && lhs._seed == rhs._seed &&
+           lhs._multiplicand == rhs._multiplicand;
   }
 
   friend constexpr bool operator!=(const MulShift &lhs, const MulShift &rhs) {
@@ -204,12 +208,12 @@ struct MulShift : public Base {
 #if __has_include(<cereal/types/base_class.hpp>)
   template <class Archive>
   void serialize(Archive &archive) {
-    archive(cereal::base_class<Base>(this), _a);
+    archive(cereal::base_class<Base>(this), _multiplicand);
   }
 #endif
 
  private:
-  std::uint64_t _a;
+  std::uint64_t _multiplicand;
 };
 
 /**
@@ -219,9 +223,10 @@ struct MulShift : public Base {
  */
 struct MulAddShift : public Base {
   /**
-   * Initialize `_a` and `_b` given the specified random `seed`.
+   * Initialize `_multiplicand` and `_summand` given the specified random
+   * `seed`.
    *
-   * `_a` and `_b` are randomly sampled from [0, 2^64 - 1] and
+   * `_multiplicand` and `_summand` are randomly sampled from [0, 2^64 - 1] and
    * [0, 2^size() - 1], respectively.
    *
    * @tparam ARGS types of additional (ignored) hash functor arguments.
@@ -234,21 +239,21 @@ struct MulAddShift : public Base {
               const std::uint64_t seed = default_seed, const ARGS &...)
       : Base(range, seed) {
     std::mt19937_64                              rnd_gen(wang64(_seed));
-    std::uniform_int_distribution<std::uint64_t> udist_a(
+    std::uniform_int_distribution<std::uint64_t> udist_multiplicand(
         0, std::numeric_limits<std::uint64_t>::max());
-    std::uniform_int_distribution<std::uint64_t> udist_b(0, size());
-    std::uint64_t                                a;
+    std::uniform_int_distribution<std::uint64_t> udist_summand(0, size());
+    std::uint64_t                                multiplicand;
     do {
-      a = udist_a(rnd_gen);
-    } while (is_even(a));
-    _a = a;
-    _b = udist_b(rnd_gen);
+      multiplicand = udist_multiplicand(rnd_gen);
+    } while (is_even(multiplicand));
+    _multiplicand = multiplicand;
+    _summand      = udist_summand(rnd_gen);
   }
 
   MulAddShift() : Base() {}
 
   /**
-   * Compute `_a * x + _b mod _m`.
+   * Compute `_multiplicand * x + _summand mod _range`.
    *
    * @tparam OBJ the object to be hashed. Presently must fit into a 64-bit
    *     register.
@@ -257,7 +262,7 @@ struct MulAddShift : public Base {
    */
   template <typename OBJ>
   constexpr std::uint64_t operator()(const OBJ &x) const {
-    return truncate(_a * x + _b);
+    return truncate(_multiplicand * x + _summand);
   }
 
   /**
@@ -267,20 +272,22 @@ struct MulAddShift : public Base {
 
   inline std::string state() const {
     std::stringstream ss;
-    ss << Base::state() << ", multiplicand: " << _a << ",  summand: " << _b;
+    ss << Base::state() << ", multiplicand: " << _multiplicand
+       << ",  summand: " << _summand;
     return ss.str();
   }
 
   friend void swap(MulAddShift &lhs, MulAddShift &rhs) {
-    std::swap(lhs._a, rhs._a);
-    std::swap(lhs._b, rhs._b);
+    std::swap(lhs._multiplicand, rhs._multiplicand);
+    std::swap(lhs._summand, rhs._summand);
     lhs.swap(rhs);
   }
 
   friend constexpr bool operator==(const MulAddShift &lhs,
                                    const MulAddShift &rhs) {
-    return lhs._m == rhs._m && lhs._seed == rhs._seed && lhs._a == rhs._a &&
-           lhs._b == rhs._b;
+    return lhs._range == rhs._range && lhs._seed == rhs._seed &&
+           lhs._multiplicand == rhs._multiplicand &&
+           lhs._summand == rhs._summand;
   }
 
   friend constexpr bool operator!=(const MulAddShift &lhs,
@@ -291,13 +298,13 @@ struct MulAddShift : public Base {
 #if __has_include(<cereal/types/base_class.hpp>)
   template <class Archive>
   void serialize(Archive &archive) {
-    archive(cereal::base_class<Base>(this), _a, _b);
+    archive(cereal::base_class<Base>(this), _multiplicand, _summand);
   }
 #endif
 
  private:
-  std::uint64_t _a;
-  std::uint64_t _b;
+  std::uint64_t _multiplicand;
+  std::uint64_t _summand;
 };
 
 std::ostream &operator<<(std::ostream &os, const Base &func) {
